879ba33c45819fe91b1ce99459862a3a943be253
Eric P. Stavarache <ericptst@gmail.com>
2017-02-09 16:46:53 +0200

    * install npm requirements

--- create.py
+++ create.py
@@ -20,7 +20,16 @@ def main():
 
     print("Importing skeleton from {}".format(skeleton_repo))
     subprocess.check_call(['git', 'clone', skeleton_repo, project_dir])
-    shutil.rmtree('{}/.git".format(project_dir)')
+    shutil.rmtree('{}/.git'.format(project_dir))
+
+    print("Globally installing babel-cli and rollup")
+    subprocess.check_call(['sudo', 'npm', 'install', '-g', 'babel-cli', 'rollup'])
+
+    subprocess.check_call(['npm', 'update'], cwd=project_dir)
+
+    subprocess.check_call(['rollup', '-c'], cwd="{}/src".format(project_dir))
+
+    print("Successfully created project {}".format(project_dir))
 
 if __name__ == "__main__":
     sys.exit(main())
