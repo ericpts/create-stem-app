27fa3f5953ce2fef8a5e8d1273bc8209d9f99618
Eric P. Stavarache <ericptst@gmail.com>
2017-02-09 17:21:14 +0200

    * Better messages

--- create.py
+++ create.py
@@ -18,18 +18,29 @@ def main():
         print("Directory {} already exists!", project_dir)
         return -1
 
-    print("Importing skeleton from {}".format(skeleton_repo))
+    print("Importing skeleton from {}\n".format(skeleton_repo))
     subprocess.check_call(['git', 'clone', skeleton_repo, project_dir])
     shutil.rmtree('{}/.git'.format(project_dir))
 
-    print("Globally installing babel-cli and rollup")
+    print("Globally installing babel-cli and rollup\n")
     subprocess.check_call(['sudo', 'npm', 'install', '-g', 'babel-cli', 'rollup'])
 
+    print("Installing requirements with npm\n")
     subprocess.check_call(['npm', 'update'], cwd=project_dir)
 
-    subprocess.check_call(['rollup', '-c'], cwd="{}/src".format(project_dir))
+    print("Compiling using rollup... ", end='', flush=True)
+    subprocess.check_call(['rollup', '-c'], cwd="{}/src".format(project_dir), stdout=open(os.devnull, "w"))
+    print("OK")
 
-    print("Successfully created project {}".format(project_dir))
+    print("\nSuccessfully created project {}".format(project_dir))
+    print("Check out the README located there")
+    print("\nInside that directory, you can run\n")
+    print("\tnpm start")
+    print("\t\tto start a simple development server\n")
+
+    print("\tcd src; rollup -c --watch")
+    print("\t\tto start rollup\n")
+
 
 if __name__ == "__main__":
     sys.exit(main())
